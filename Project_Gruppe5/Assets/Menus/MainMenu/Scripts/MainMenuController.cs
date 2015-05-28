07cbbca1de7f9fca98a3623371861a16e296c7ae
tomwim <samo.miw@gmail.com>
2015-05-28 12:30:12 +0200

    Add MainMenu dummy

---
+++ Project_Gruppe5/Assets/Menus/MainMenu/Scripts/MainMenuController.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuController : Singleton<MainMenuController> {
+
+	public void LoadLevel()
+	{
+		LoadingController.Instance.LoadLevel(LoadingController.Level.TEST);
+	}
+
+}
