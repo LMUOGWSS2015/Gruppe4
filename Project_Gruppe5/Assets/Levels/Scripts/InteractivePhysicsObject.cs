b7fd992451018b3d8c60a634d9ccf87d518f8d8c
tomwim <samo.miw@gmail.com>
2015-05-28 12:40:28 +0200

    Add InteractivePhysicsObject to take physics in consideration

---
+++ Project_Gruppe5/Assets/Levels/Scripts/InteractivePhysicsObject.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InteractivePhysicsObject : InteractiveObject {
+
+	[HideInInspector]
+	public Rigidbody rb;
+	
+	void Start() 
+	{
+		rb = GetComponent<Rigidbody>();
+		if(rb != null) {
+			StartMe ();
+			if(noTrigger)
+				Activate();
+		} else {
+			Debug.LogWarning("No Rigidbody attached!");
+		}
+	}
+
+	//overrides InteractiveObject.Update()
+	void Update() {}
+	
+	void FixedUpdate() 
+	{
+		if(isActivated) {
+			DoActivation ();
+		}
+		
+		if(isDeactivated && !singleUse) {
+			DoDeactivation ();
+		}
+	}
+}
