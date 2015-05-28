256dfd79a350c201a1dbc0bf696c8f2f6899bdc0
tomwim <samo.miw@gmail.com>
2015-05-28 12:36:05 +0200

    Add simple Player-Script (Singleton)

---
+++ Project_Gruppe5/Assets/Player/Scripts/Player.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : Singleton<Player> {
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+
+	public void Kill() 
+	{
+		Debug.Log ("Player was killed!");
+	}
+}
