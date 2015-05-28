b6ad5cb6c1f7c339b7165400fa1ddd7838577b3c
tomwim <samo.miw@gmail.com>
2015-05-28 12:19:57 +0200

    Add LoadingController and MainController

---
+++ Project_Gruppe5/Assets/Scripts/LoadingController.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadingController : Singleton<LoadingController> {
+
+	public enum Level 
+	{
+		PLAYER = -1,
+		TEST = 0
+	}
+
+	public enum Menu 
+	{
+		MAIN = 0
+	}
+
+	public override void StartMe()
+	{
+		DontDestroyOnLoad (this);
+	}
+
+	public void LoadMenu (Menu menu)
+	{
+		string sceneName;
+
+		switch (menu) {
+		case Menu.MAIN:
+			sceneName = "MainMenu";
+			break;
+		default:
+			sceneName = "";
+			break;
+		}
+
+		StartCoroutine (Load(sceneName));
+	}
+
+	public void LoadLevel (Level level)
+	{
+		string levelName = "Level_" + (int)level;
+		StartCoroutine(Load(levelName));
+	}
+
+	private IEnumerator Load(string sceneName)
+	{
+		AsyncOperation async = Application.LoadLevelAsync(sceneName);
+		yield return async;
+		Debug.Log("Loading of " + sceneName + " complete.");
+	}
+
+}
---
+++ Project_Gruppe5/Assets/Scripts/MainController.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainController : Singleton<MainController> {
+
+	public enum State {
+		PLAYER_TEST = -2,
+		MAIN_MENU = -1,
+		START = 0,
+		TEST_LEVEL = 1
+	}
+
+	public State state;
+
+	public LoadingController loadingControllerPrefab;
+	private LoadingController loadingController;
+
+	// Use this for initialization
+	void Start () 
+	{
+		DontDestroyOnLoad (this);
+
+		InitControllers();
+		InitGame();
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if(Input.GetKeyDown (KeyCode.L)) {
+			loadingController.LoadLevel(LoadingController.Level.TEST);
+		}
+
+		if(Input.GetKeyDown (KeyCode.M)) {
+			loadingController.LoadMenu(LoadingController.Menu.MAIN);
+		}
+	}
+
+	private void InitControllers()
+	{
+		loadingController = GameObject.Instantiate(loadingControllerPrefab);
+		loadingController.StartMe();
+	}
+
+	private void InitGame()
+	{
+		switch(state) {
+		case State.MAIN_MENU:
+			loadingController.LoadMenu(LoadingController.Menu.MAIN);
+			break;
+		case State.TEST_LEVEL:
+			loadingController.LoadLevel(LoadingController.Level.TEST);
+			break;
+		case State.PLAYER_TEST:
+			loadingController.LoadLevel(LoadingController.Level.PLAYER);
+			break;
+		}
+	}
+}
