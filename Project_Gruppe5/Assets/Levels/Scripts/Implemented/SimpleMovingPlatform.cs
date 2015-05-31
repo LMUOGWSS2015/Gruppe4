d98dd95bb5f7b92b74c8810ede35109f2842b08c
tomwim <samo.miw@gmail.com>
2015-05-31 15:12:08 +0200

    Add SimpleMovingPlatform (with pingpong functionality)

---
+++ Project_Gruppe5/Assets/Levels/Scripts/Implemented/SimpleMovingPlatform.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SimpleMovingPlatform : InteractivePhysicsObject {
+	
+	public bool pingPong;
+	public float speed;
+	public Direction direction;
+	
+	private Vector3 startPosition;
+	private Vector3 endPosition;
+
+	public enum Direction {
+		RIGHT,
+		LEFT,
+		BACK,
+		FORWARD,
+		UP,
+		DOWN
+	}
+	
+	// Use this for initialization
+	public override void StartMe ()
+	{
+		startPosition = transform.position;
+		endPosition = transform.position + GetDistanceVector();
+	}
+
+	private Vector3 GetDistanceVector() 
+	{
+		Vector3 distanceVector;
+		switch(direction) {
+		case Direction.RIGHT:
+			distanceVector = transform.right * transform.localScale.x;
+			break;
+		case Direction.LEFT:
+			distanceVector =  -transform.right * transform.localScale.x;
+			break;
+		case Direction.BACK:
+			distanceVector =  -transform.forward * transform.localScale.z;
+			break;
+		case Direction.FORWARD:
+			distanceVector =  transform.forward * transform.localScale.z;
+			break;
+		case Direction.UP:
+			distanceVector =  transform.up * transform.localScale.y;
+			break;
+		case Direction.DOWN:
+			distanceVector =  -transform.up * transform.localScale.y;
+			break;
+		default:
+			distanceVector =  -transform.right * transform.localScale.x;
+			break;
+		}
+
+		return distanceVector;
+	}
+
+	public override void DoActivation()
+	{
+		if(pingPong && transform.position == endPosition) {
+			isActivated = false;
+			isDeactivated = true;
+		}
+		rb.MovePosition(Vector3.MoveTowards(transform.position, endPosition, Time.deltaTime * speed));
+	}
+	
+	public override void DoDeactivation()
+	{
+		if(pingPong && transform.position == startPosition) {
+			isActivated = true;
+			isDeactivated = false;
+		}
+		rb.MovePosition(Vector3.MoveTowards(transform.localPosition, startPosition, Time.deltaTime * speed));
+	}
+}
