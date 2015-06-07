8d27ac92ca2f4bb13b57b1b7fc44b8b3252ae419
tomwim <samo.miw@gmail.com>
2015-06-07 14:57:16 +0200

    Add PlayerUtils script for stuff like kill and respawn

---
+++ Project_Gruppe5/Assets/Player/Scripts/PlayerUtils.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerUtils : Singleton<PlayerUtils> {
+
+	public Transform player;
+
+	public Transform respawnPoint {
+		set;
+		get;
+	}
+
+	public void Respawn()
+	{
+		player.position = respawnPoint.position;
+		player.rotation = respawnPoint.rotation;
+	}
+
+	public void Kill() 
+	{
+		Respawn();
+	}
+
+}
