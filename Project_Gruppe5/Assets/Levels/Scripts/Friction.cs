47e18650ee180e039a4d2c2625d1326eb880676e
Paaadi <Paaadi@Paaadi.local>
2015-06-08 16:01:01 +0200

    Friction der MovingPlatform angepasst

---
+++ Project_Gruppe5/Assets/Levels/Scripts/Friction.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Friction : MonoBehaviour {
+	
+	void Start () {
+		Transform parent = transform.parent;
+
+		transform.localScale = new Vector3 (1.0f - (1.0f / (parent.localScale.x / 0.5f)), transform.localScale.y, 1.0f - (1.0f / (parent.localScale.z / 0.5f)));
+	}
+}
