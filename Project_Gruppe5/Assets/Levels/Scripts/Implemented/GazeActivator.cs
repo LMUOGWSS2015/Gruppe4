f1682edb40295ef57e1c6f4b485551bbab49d6c9
Esteba <esteba@gmx.de>
2015-06-14 16:33:30 +0200

    GazeActivator-Skript zum temporären Aktivieren von Gaze-Events über die Tastatur; Testlevel mit ausfahrbaren Plattformen

---
+++ Project_Gruppe5/Assets/Levels/Scripts/Implemented/GazeActivator.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GazeActivator : ActivatingObject {
+
+	public KeyCode key = KeyCode.Space;
+
+	public override void Update() {
+		base.Update ();
+		if (Input.GetKeyDown(key)) {
+			if (isActivated) {
+				Deactivated();
+			} else {
+				Activated();
+			}
+		}
+	}
+
+	public override void Activated()
+	{
+		base.Activated();
+	}
+
+	public override void Deactivated()
+	{
+		base.Deactivated();
+	}
+}
