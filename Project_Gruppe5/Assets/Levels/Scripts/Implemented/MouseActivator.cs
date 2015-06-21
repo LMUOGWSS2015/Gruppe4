139f1ab5457b93bcfa79385b93b8b665ef2a73b9
Esteba <esteba@gmx.de>
2015-06-21 11:25:26 +0200

    MouseActivator-Skript als Platzhalter für Gaze-Activation; Prefab für den Gaze-Kristall

---
+++ Project_Gruppe5/Assets/Levels/Scripts/Implemented/MouseActivator.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class MouseActivator : ActivatingObject {
+
+	public bool deactivateOnExit = false;
+
+	public override void Activated()
+	{
+		base.Activated();
+	}
+
+	public override void Deactivated()
+	{
+		base.Deactivated();
+	}
+
+	public void OnMouseEnter() {
+		Activated ();
+	}
+
+	public void OnMouseExit() {
+		if (deactivateOnExit) {
+			Deactivated();
+		}
+	}
+
+}
