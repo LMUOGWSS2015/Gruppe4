c6c0edc1196f014c6d2702f4fafc6dbed0d2df88
tomwim <samo.miw@gmail.com>
2015-07-01 22:25:42 +0200

    Pushback script for enemies

---
+++ Project_Gruppe5/Assets/Levels/Scripts/Implemented/Pushback.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pushback : MyMonoBehaviour {
+
+	public float power = 800.0f;
+
+	void OnCollisionEnter (Collision other)
+	{
+		if(other.gameObject.tag == "Player") {
+			Vector3 pushbackPoint = new Vector3(other.contacts[0].normal.x, other.transform.position.y - 1, other.contacts[0].normal.z);
+
+			Player.Instance.gameObject.GetComponent<Rigidbody>().AddExplosionForce(power, pushbackPoint, 20.0f);
+		}
+	}
+}
