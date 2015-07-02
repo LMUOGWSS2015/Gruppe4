4263d57ef442e2b9200717945644b384bddd0a5b
tomwim <samo.miw@gmail.com>
2015-07-02 23:31:36 +0200

    Attacking Script + pushback increased

---
+++ Project_Gruppe5/Assets/Levels/Scripts/Implemented/Aggressor.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Aggressor : MonoBehaviour {
+
+	public float timeToTarget = 1.0f;
+
+	private bool isAttacking;
+
+	public void Attack (Transform target)
+	{
+		Vector3 position = target.position;
+		position = new Vector3 (position.x, transform.position.y, position.z);
+
+		StartCoroutine (Attacking(position));
+	}
+
+	void OnCollisionEnter (Collision other)
+	{
+		if(other.transform.tag == "Player")
+			isAttacking = false;
+	}
+
+	private IEnumerator Attacking (Vector3 targetPosition) 
+	{
+		isAttacking = true;
+		float currentLerpTime = 0;
+		float lerpTime = timeToTarget;
+
+		Vector3 startPos = transform.position;
+
+		while(isAttacking) {
+
+			currentLerpTime += Time.deltaTime;
+			if (currentLerpTime > lerpTime) {
+				currentLerpTime = lerpTime;
+			}
+
+			float t = currentLerpTime / lerpTime;
+			t = 1f - Mathf.Cos(t * Mathf.PI * 0.5f);
+			Vector3 newPos = Vector3.Lerp(startPos, targetPosition, t);
+			transform.position = newPos;
+
+			if(transform.position == targetPosition)
+				isAttacking = false;
+
+			yield return null;
+		}
+	}
+
+}
