015c14c92b01150ba3a9fb4f2e01435a91f58969
Paaadi <Paaadi@Paaadi.local>
2015-07-07 11:31:31 +0200

    nochmal verbesserung für pushback

--- Project_Gruppe5/Assets/Levels/Scripts/Implemented/Pushback.cs
+++ Project_Gruppe5/Assets/Levels/Scripts/Implemented/Pushback.cs
@@ -11,7 +11,7 @@ public class Pushback : MyMonoBehaviour {
 			//Vector3 pushbackPoint = new Vector3(other.contacts[0].normal.x * -1, 0.0f, other.contacts[0].normal.z * -1);
 			//Player.Instance.gameObject.GetComponent<Rigidbody>().AddExplosionForce(power, pushbackPoint, 20.0f);
 
-			Vector3 pushback = new Vector3(other.contacts[0].normal.x * -1 * power, 0.0f, other.contacts[0].normal.z * -1 * power);
+			Vector3 pushback = new Vector3(other.contacts[0].normal.x * -1 * power, other.contacts[0].normal.y * -1 * (power), other.contacts[0].normal.z * -1 * power);
 			Player.Instance.Hit(pushback);
 		}
 	}	
