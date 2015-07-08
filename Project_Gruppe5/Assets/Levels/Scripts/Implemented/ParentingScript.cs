b9dedc88139b357c0f48ceb8c08fa0936bc1756f
EEnglmeier <elias_englmeier@yahoo.de>
2015-07-08 19:40:34 +0200

    ForestLevel rotating planks fixed + ForestRestartContent

---
+++ Project_Gruppe5/Assets/Levels/Scripts/Implemented/ParentingScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ParentingScript : MonoBehaviour {
+
+
+	public GameObject parent;
+
+	void OnCollisionEnter (Collision col)
+	{
+		if (col.gameObject.tag == "Player"){ 
+			col.gameObject.transform.SetParent(parent.transform);
+			}
+	}
+	void OnCollisionExit(Collision col) {
+		if (col.gameObject.tag == "Player") {
+			col.gameObject.transform.parent = null;
+		}
+	}
+}
