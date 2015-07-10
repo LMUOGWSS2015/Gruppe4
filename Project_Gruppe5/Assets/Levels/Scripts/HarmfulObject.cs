9d0b9c67706ff622bbca529e4fd0286b66050c1f
Paaadi <Paaadi@Paaadi.local>
2015-07-10 23:41:38 +0200

    ForestLevel MovingPlattform Parcour

--- Project_Gruppe5/Assets/Levels/Scripts/HarmfulObject.cs
+++ Project_Gruppe5/Assets/Levels/Scripts/HarmfulObject.cs
@@ -6,7 +6,6 @@ public class HarmfulObject : MyMonoBehaviour {
 	void OnCollisionEnter(Collision other) 
 	{
 		if(other.transform.tag == "Player" && !Player.Instance.isDead()) {
-			Debug.Log ("Kill");
 			Player.Instance.KillByObject();
 		}
 	}
