485d0ceaa134379c4a62a794de418c18bd961937
tomwim <samo.miw@gmail.com>
2015-07-13 20:36:24 +0200

    Add missing script

---
+++ Project_Gruppe5/Assets/Levels/Scripts/Thorns/VanishingSound.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VanishingSound : MonoBehaviour {
+
+	private bool played;
+
+	public void Enable()
+	{
+		if(!played) {
+			GetComponent<AudioSource>().Play();
+			played = true;
+		}
+	}
+
+}
