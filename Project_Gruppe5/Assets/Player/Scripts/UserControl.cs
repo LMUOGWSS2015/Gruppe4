9a9c48f8ed50adee3c537237c66216a6e41c1743
Paaadi <Paaadi@Paaadi.local>
2015-07-14 20:21:46 +0200

    Camera bug bei Camera ohne dof gefixt

--- Project_Gruppe5/Assets/Player/Scripts/UserControl.cs
+++ Project_Gruppe5/Assets/Player/Scripts/UserControl.cs
@@ -14,22 +14,24 @@ public class UserControl : MonoBehaviour
 	private bool jump;                      // true if the User pressed the jump button
 
 	private void Start() {
-		// get the transform of the main camera
-		if (Camera.main != null) {
-			cam = Camera.main.transform;
-		}
-		else {
-			Debug.LogWarning(
-				"Warning: no main camera found. Third person character needs a Camera tagged \"MainCamera\", for camera-relative controls.");
-			// we use self-relative controls in this case, which probably isn't what the user wants, but hey, we warned them!
-		}
-
 		// get the third person character ( this should never be null due to require component )
 		player = GetComponent<Player>();
 	}
 	
 	
 	private void Update() {
+		if (cam == null) {
+			// get the transform of the main camera
+			if (Camera.main != null) {
+				cam = Camera.main.transform;
+			}
+			else {
+				Debug.LogWarning(
+					"Warning: no main camera found. Third person character needs a Camera tagged \"MainCamera\", for camera-relative controls.");
+				// we use self-relative controls in this case, which probably isn't what the user wants, but hey, we warned them!
+			}
+		}
+
 		if (!jump) {
 			jump = InputManager.Jump();
 		}
