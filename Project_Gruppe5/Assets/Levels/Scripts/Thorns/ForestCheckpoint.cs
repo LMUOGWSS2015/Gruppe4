49708eb7affbfc0141f062c757a0cd8b6e5e3dbf
tomwim <samo.miw@gmail.com>
2015-07-14 19:38:47 +0200

    Add checkpoint in forest

---
+++ Project_Gruppe5/Assets/Levels/Scripts/Thorns/ForestCheckpoint.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ForestCheckpoint : MonoBehaviour {
+
+	public LightCircle lightCircle;
+
+	void OnTriggerEnter (Collider other)
+	{
+		if(other.tag == "Player") {
+			if(!lightCircle.gameObject.activeSelf) {
+				lightCircle.gameObject.SetActive(true);
+				lightCircle.DoActivation();
+			}
+
+		}
+	}
+}
---
+++ Project_Gruppe5/Assets/Levels/Scripts/Thorns/ForestDeadZone.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ForestDeadZone : MonoBehaviour {
+
+	public LightCircle lCircle;
+
+	void OnTriggerEnter (Collider other)
+	{
+		if(other.tag == "Player") {
+			lCircle.DoActivation ();
+		}
+	}
+}
